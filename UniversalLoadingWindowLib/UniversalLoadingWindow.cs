4378dc31c692c0451b06fc463c453908866e43d8
VladislavZverincev <hopmxod007@mail.ru>
2021-10-12 23:06:04 +0700

    Добавьте файлы проекта.

---
+++ UniversalLoadingWindowLib/UniversalLoadingWindow.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace UniversalLoadingWindowLib
+{
+    /// <summary>
+    /// Class for build Universal Loading Window with Title, Annotation, 
+    /// animated bubbles, and percents of Progress (optional). Height="160" Width="400"
+    /// </summary>
+    public class UniversalLoadingWindow
+    {
+        #region Fields
+        private LoadingWindowViewModel viewModel = new LoadingWindowViewModel();
+        WindowLoading wl;
+        #endregion Fields
+        #region Properties
+        private SolidColorBrush MainBrush { get; set; }
+        private Color ElementColor { get; set; }
+        public LoadingWindowViewModel ViewModel
+        {
+            get { return viewModel; }
+            set { viewModel = value; }
+        }
+        #endregion Properties
+        #region Constructors
+        /// <summary>
+        /// Default Constructor with default gray colors of LoadingWindow
+        /// </summary>
+        public UniversalLoadingWindow()
+        {
+
+        }
+        /// <summary>
+        /// Constructor with configurated LoadingWindow Parameters
+        /// </summary>
+        /// <param name="_title"> Title</param>
+        /// <param name="_annotation"> Annotation</param>
+        /// <param name="_mainBrush"> Brush of main background</param>
+        /// <param name="_elementColor"> Color of animated bubbles</param>
+        /// <param name="_foregroundTitle">Title's font color (and color of line and percents)</param>
+        /// <param name="_foregroundAnnotation"> Annotation's font color</param>
+        public UniversalLoadingWindow(string _title, string _annotation, SolidColorBrush _mainBrush, Color _elementColor, SolidColorBrush _foregroundTitle, SolidColorBrush _foregroundAnnotation)
+        {
+            MainBrush = _mainBrush;
+            ElementColor = _elementColor;
+            ViewModel = new LoadingWindowViewModel(_title, _annotation, MainBrush, ElementColor, _foregroundTitle, _foregroundAnnotation);
+        }
+        #endregion Constructors
+        #region Methods
+        /// <summary>
+        /// Show Loading Window
+        /// </summary>
+        public void Show()
+        {
+            if (ViewModel != null)
+            {
+                wl = new WindowLoading();
+                wl.DataContext = this;
+                wl.Show();
+            }
+        }
+        /// <summary>
+        /// Show Loading Window (Modal)
+        /// </summary>
+        public void ShowDialog()
+        {
+            if (ViewModel != null)
+            {
+                wl = new WindowLoading();
+                wl.DataContext = this;
+                wl.ShowDialog();
+            }
+        }
+        /// <summary>
+        /// Close Loading Window
+        /// </summary>
+        public void Close()
+        {
+            if (wl != null)
+            {
+                wl.Close();
+            }
+        }
+        /// <summary>
+        /// Set Title of Loading window. This is show in the top of window.
+        /// </summary>
+        /// <param name="newTitle"> If not used set Empty.String </param>
+        public void SetTitle(string newTitle)
+        {
+            if (ViewModel != null)
+            {
+                ViewModel.Title = newTitle;
+            }
+        }
+        /// <summary>
+        /// Set Annotation of Loading.
+        /// </summary>
+        /// <param name="newAnnotation">  if not used set Empty.String </param>
+        public void SetAnnotation(string newAnnotation)
+        {
+            if (ViewModel != null)
+            {
+                ViewModel.Annotation = newAnnotation;
+            }
+        }
+        /// <summary>
+        /// Set Percents of Progress, this is shows inside animated bubbles, default value "0". When value "0" percents not shown
+        /// </summary>
+        /// <param name="newPercent"> byte value from 0 to 100 </param>
+        public void SetPercentProgress(byte newPercent)
+        {
+            if (ViewModel != null)
+            {
+                ViewModel.PercentsProgress = newPercent;
+            }
+        }
+        #endregion Methods
+    }
+}
---
+++ UniversalLoadingWindowLib/ViewModel/LoadingWindowViewModel.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Timers;
+using System.Windows.Threading;
+
+namespace UniversalLoadingWindowLib
+{
+    public class LoadingWindowViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+        string title;
+        string annotation;
+        byte percentsProgress = 0;
+        //Brushes
+        SolidColorBrush mainBrush;
+        SolidColorBrush foreground_title;
+        SolidColorBrush foreground_annotation;
+        SolidColorBrush elementBrush;
+        SolidColorBrush elementBrush6;
+        SolidColorBrush elementBrush5;
+        SolidColorBrush elementBrush4;
+        SolidColorBrush elementBrush3;
+        SolidColorBrush elementBrush2;
+        SolidColorBrush elementBrush1;
+        SolidColorBrush elementBrush0;
+        //
+        private static System.Timers.Timer animValueTimer;
+        #endregion Fields
+
+        #region Properties
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Title"));
+            }
+        }
+        public string Annotation
+        {
+            get { return annotation; }
+            set
+            {
+                annotation = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Annotation"));
+            }
+        }
+        public byte PercentsProgress
+        {
+            get { return percentsProgress; }
+            set
+            {
+                if(value >= 0 && value <= 100)
+                {
+                    percentsProgress = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PercentsProgress"));
+                }
+            }
+        }
+        public SolidColorBrush MainBrush
+        {
+            get { return mainBrush; }
+            set
+            {
+                mainBrush = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MainBrush"));
+            }
+        }
+        public SolidColorBrush Foreground_Title
+        {
+            get { return foreground_title; }
+            set
+            {
+                foreground_title = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Foreground_Title"));
+            }
+        }
+        public SolidColorBrush Foreground_Annotation
+        {
+            get { return foreground_annotation; }
+            set
+            {
+                foreground_annotation = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Foreground_Annotation"));
+            }
+        }
+        public SolidColorBrush ElementBrush 
+        { 
+            get { return elementBrush; } 
+            set { elementBrush = value; 
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ElementBrush")); }
+        }
+        public SolidColorBrush ElementBrush6
+        {
+            get { return elementBrush6; }
+            set
+            {
+                elementBrush6 = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ElementBrush6"));
+            }
+        }
+        public SolidColorBrush ElementBrush5
+        {
+            get { return elementBrush5; }
+            set
+            {
+                elementBrush5 = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ElementBrush5"));
+            }
+        }
+        public SolidColorBrush ElementBrush4
+        {
+            get { return elementBrush4; }
+            set
+            {
+                elementBrush4 = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ElementBrush4"));
+            }
+        }
+        public SolidColorBrush ElementBrush3
+        {
+            get { return elementBrush3; }
+            set
+            {
+                elementBrush3 = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ElementBrush3"));
+            }
+        }
+        public SolidColorBrush ElementBrush2
+        {
+            get { return elementBrush2; }
+            set
+            {
+                elementBrush2 = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ElementBrush2"));
+            }
+        }
+        public SolidColorBrush ElementBrush1
+        {
+            get { return elementBrush1; }
+            set
+            {
+                elementBrush1 = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ElementBrush1"));
+            }
+        }
+        public SolidColorBrush ElementBrush0
+        {
+            get { return elementBrush0; }
+            set
+            {
+                elementBrush0 = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ElementBrush0"));
+            }
+        }
+        #endregion Properties
+
+        #region Constructors
+        public LoadingWindowViewModel(string _title, string _annotation, 
+            SolidColorBrush _mainBrush, Color _elementColor, SolidColorBrush _foregroundTitle, 
+            SolidColorBrush _foregroundAnnotation)
+        {
+            if(_mainBrush != null && _elementColor != null && _title != null && _annotation != null && _foregroundTitle != null)
+            {
+                Title = _title;
+                Annotation = _annotation;
+                MainBrush = _mainBrush;
+                Foreground_Title = _foregroundTitle;
+                Foreground_Annotation = _foregroundAnnotation;
+                ElementBrush = new SolidColorBrush(new Color { A = 255, R = _elementColor.R, G = _elementColor.G, B = _elementColor.B });
+                ElementBrush6 = new SolidColorBrush(new Color { A = 223, R = _elementColor.R, G = _elementColor.G, B = _elementColor.B });
+                ElementBrush5 = new SolidColorBrush(new Color { A = 191, R = _elementColor.R, G = _elementColor.G, B = _elementColor.B });
+                ElementBrush4 = new SolidColorBrush(new Color { A = 159, R = _elementColor.R, G = _elementColor.G, B = _elementColor.B });
+                ElementBrush3 = new SolidColorBrush(new Color { A = 128, R = _elementColor.R, G = _elementColor.G, B = _elementColor.B });
+                ElementBrush2 = new SolidColorBrush(new Color { A = 97, R = _elementColor.R, G = _elementColor.G, B = _elementColor.B });
+                ElementBrush1 = new SolidColorBrush(new Color { A = 66, R = _elementColor.R, G = _elementColor.G, B = _elementColor.B });
+                ElementBrush0 = new SolidColorBrush(new Color { A = 35, R = _elementColor.R, G = _elementColor.G, B = _elementColor.B });
+            }
+            else
+            {
+                Title = "Title";
+                Annotation = "Annotation";
+                SetDefaultColors();
+            }
+            SetAnimValueTimer();
+        }
+        public LoadingWindowViewModel()
+        {
+            Title = "Title";
+            Annotation = "Annotation";
+            SetDefaultColors();
+            SetAnimValueTimer();
+        }
+        #endregion Constructors
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            RefreshAnimValues();
+        }
+        #endregion Events
+
+        #region Methods
+        private void SetDefaultColors()
+        {
+            Color defaultElementColor = new Color { A = 255, R = 128, G = 128, B = 128 };
+            MainBrush = new SolidColorBrush(new Color { A = 255, R = 100, G = 100, B = 100 });
+            Foreground_Title = new SolidColorBrush(new Color { A = 255, R = defaultElementColor.R, G = defaultElementColor.G, B = defaultElementColor.B });
+            Foreground_Annotation = new SolidColorBrush(new Color { A = 255, R = defaultElementColor.R, G = defaultElementColor.G, B = defaultElementColor.B });
+            ElementBrush = new SolidColorBrush(new Color { A = 255, R = defaultElementColor.R, G = defaultElementColor.G, B = defaultElementColor.B });
+            ElementBrush6 = new SolidColorBrush(new Color { A = 223, R = defaultElementColor.R, G = defaultElementColor.G, B = defaultElementColor.B });
+            ElementBrush5 = new SolidColorBrush(new Color { A = 191, R = defaultElementColor.R, G = defaultElementColor.G, B = defaultElementColor.B });
+            ElementBrush4 = new SolidColorBrush(new Color { A = 159, R = defaultElementColor.R, G = defaultElementColor.G, B = defaultElementColor.B });
+            ElementBrush3 = new SolidColorBrush(new Color { A = 128, R = defaultElementColor.R, G = defaultElementColor.G, B = defaultElementColor.B });
+            ElementBrush2 = new SolidColorBrush(new Color { A = 97, R = defaultElementColor.R, G = defaultElementColor.G, B = defaultElementColor.B });
+            ElementBrush1 = new SolidColorBrush(new Color { A = 66, R = defaultElementColor.R, G = defaultElementColor.G, B = defaultElementColor.B });
+            ElementBrush0 = new SolidColorBrush(new Color { A = 35, R = defaultElementColor.R, G = defaultElementColor.G, B = defaultElementColor.B });
+        }
+        private SolidColorBrush GetShiftColorBrush(SolidColorBrush oldBrush)
+        {
+            if(oldBrush != null && oldBrush is SolidColorBrush)
+            {
+                Color newColor = oldBrush.Color;
+                if (oldBrush.Color.A == 255) { newColor.A = 223; }
+                if (oldBrush.Color.A == 223) { newColor.A = 191; }
+                if (oldBrush.Color.A == 191) { newColor.A = 159; }
+                if (oldBrush.Color.A == 159) { newColor.A = 128; }
+                if (oldBrush.Color.A == 128) { newColor.A = 97; }
+                if (oldBrush.Color.A == 97) { newColor.A = 66; }
+                if (oldBrush.Color.A == 66) { newColor.A = 35; }
+                if (oldBrush.Color.A == 35) { newColor.A = 255; }
+                return new SolidColorBrush(newColor);
+            }
+            else { return null; }
+        }
+        private void SetAnimValueTimer()
+        {
+            animValueTimer = new System.Timers.Timer(80);
+            animValueTimer.Elapsed += OnTimedEvent;
+            animValueTimer.Enabled = true;
+        }
+        private void RefreshAnimValues()
+        {
+            Application.Current.Dispatcher.Invoke(() => ElementBrush = GetShiftColorBrush(ElementBrush));
+            Application.Current.Dispatcher.Invoke(() => ElementBrush6 = GetShiftColorBrush(ElementBrush6));
+            Application.Current.Dispatcher.Invoke(() => ElementBrush5 = GetShiftColorBrush(ElementBrush5));
+            Application.Current.Dispatcher.Invoke(() => ElementBrush4 = GetShiftColorBrush(ElementBrush4));
+            Application.Current.Dispatcher.Invoke(() => ElementBrush3 = GetShiftColorBrush(ElementBrush3));
+            Application.Current.Dispatcher.Invoke(() => ElementBrush2 = GetShiftColorBrush(ElementBrush2));
+            Application.Current.Dispatcher.Invoke(() => ElementBrush1 = GetShiftColorBrush(ElementBrush1));
+            Application.Current.Dispatcher.Invoke(() => ElementBrush0 = GetShiftColorBrush(ElementBrush0));
+        }
+        #endregion Methods
+    }
+}
