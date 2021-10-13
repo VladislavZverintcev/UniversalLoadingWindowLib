3b3ecdc05263936098545044d59ac37d2ee4e18e
VladislavZverincev <hopmxod007@mail.ru>
2021-10-13 14:44:54 +0700

    Has been deleted not used references

--- UniversalLoadingWindowLib/ViewModel/LoadingWindowViewModel.cs
+++ UniversalLoadingWindowLib/ViewModel/LoadingWindowViewModel.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using System.Timers;
-using System.Windows.Threading;
 
 namespace UniversalLoadingWindowLib
 {
